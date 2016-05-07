1accf428712f796fae0860982b1dc0f0c3e3b197
Nathan Conley <nateconley123@gmail.com>
2016-05-07 12:32:16 -0700

    Finished JavaScript leap

---
+++ javascript/leap/leap.js
@@ -0,0 +1,15 @@
+function Year(year) {
+	this.isLeap = function () {
+		if (year % 400 === 0) {
+			return true;
+		} else if (year % 100 === 0) {
+			return false;
+		} else if (year % 4 === 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
+module.exports = Year;
